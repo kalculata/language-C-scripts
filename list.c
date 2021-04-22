241d95cc9c7d9b9828379984f76c007ac9eb3245
kalculata <nprincehuzaifa@gmail.com>
2021-04-22 16:01:53 +0200

    add function 'List *initialize_list' in file 'list.c'

--- list.c
+++ list.c
@@ -13,6 +13,22 @@ typedef struct List
     Item *firstItem;
 } List;
 
+List *initialize_list();
+
 int main()
 {
+}
+
+List *initialize_list()
+{
+    Item *initial_item = malloc(sizeof(Item *));
+    List *initialized_list = malloc(sizeof(List *));
+
+    initial_item->isInitialItem = 1;
+    initial_item->value = 0;
+    initial_item->next = NULL;
+
+    initialized_list->firstItem = initial_item;
+
+    return initialized_list;
 }
