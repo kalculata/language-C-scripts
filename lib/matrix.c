b8b59a16019197ccde844ef605e40f53bda81aff
kalculata <nprincehuzaifa@gmail.com>
2021-04-30 16:51:11 +0200

    modify all functions in matrix.c to accept custom dimensions

--- lib/matrix.c
+++ lib/matrix.c
@@ -2,43 +2,43 @@
 #include <string.h>
 #include "io.c"
 
-void ones_matrix(int size, int matrix[size][size]);
-void printf_matrix(int size, int matrix[size][size]);
-void input_matrix(int size, int matrix[size][size]);
+void ones_matrix(int sizeX, int sizeY, int matrix[sizeX][sizeY]);
+void printf_matrix(int sizeX, int sizeY, int matrix[sizeX][sizeY]);
+void input_matrix(int sizeX, int sizeY, int matrix[sizeX][sizeY]);
 
-void ones_matrix(int size, int matrix[size][size])
+void ones_matrix(int sizeX, int sizeY, int matrix[sizeX][sizeY])
 {
     int i, j;
-    for (i = 0; i < size; i++)
+    for (i = 0; i < sizeX; i++)
     {
-        for (j = 0; j < size; j++)
+        for (j = 0; j < sizeY; j++)
         {
             matrix[i][j] = 1;
         }
     }
 }
 
-void printf_matrix(int size, int matrix[size][size])
+void printf_matrix(int sizeX, int sizeY, int matrix[sizeX][sizeY])
 {
     int i, j;
-    for (i = 0; i < size; i++)
+    for (i = 0; i < sizeX; i++)
     {
-        for (j = 0; j < size; j++)
+        for (j = 0; j < sizeY; j++)
         {
-            printf("%5i", matrix[i][j]);
+            printf("    %i", matrix[i][j]);
         }
         printf("\n");
     }
 }
 
-void input_matrix(int size, int matrix[size][size])
+void input_matrix(int sizeX, int sizeY, int matrix[sizeX][sizeY])
 {
     int i, j;
     char input_message[100];
 
-    for (i = 0; i < size; i++)
+    for (i = 0; i < sizeX; i++)
     {
-        for (j = 0; j < size; j++)
+        for (j = 0; j < sizeY; j++)
         {
             sprintf(input_message, "Matrix[%i][%i]: ", i + 1, j + 1);
             matrix[i][j] = inputInt(input_message);
